21c828adc9a0c7015c0572530636b7a9918b0a52
niteshthapa <nitesh_thapa@ymail.com>
2022-10-20 16:43:08 +0530

    Add files via upload

--- src/component/context/products/ProductReducer.js
+++ src/component/context/products/ProductReducer.js
@@ -1,1 +1,22 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { GET_PRODUCT } from "../ProductType";
+export const initialValue = {
+    allproducts: [],
+    loading: true
+  }
+const ProductReducer = (state, action) => {
+    const {type,payload} = action;
+    switch (action.type) {
+        case GET_PRODUCT:
+            return {
+                ...state,
+                allproducts: payload,
+                loading:false
+            }
 
+        default: return state
+    }
+
+}
+
+export default ProductReducer;
